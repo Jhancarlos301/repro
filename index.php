0edfd85ff773d39e580c5fc3c33c111437321c68
Jhancarlos301 <112730654+Jhancarlos301@users.noreply.github.com>
2023-03-10 09:48:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
+$url = "https://pixeldrain.com/api/file/rxvomvuEE";
+$key = "my_secret_key"; // Clave secreta para cifrar la URL
 
-#VIDEO
-$video = 'https://pixeldrain.com/api/file/rxvomvuE';
-$videoData = base64_encode(file_get_contents($video));
-$src_video = 'data: '.mime_content_type($video).';base64,'.$videoData;
+$cipher = "AES-128-CBC"; // Algoritmo de cifrado
+$ivlen = openssl_cipher_iv_length($cipher); // Longitud del vector de inicialización
+$iv = openssl_random_pseudo_bytes($ivlen); // Vector de inicialización generado aleatoriamente
 
-echo 
-'
-	<video width="100%" height="100%" controlslist="nodownload" controls>
-		<source src="'. $src_video. '" type="video/mp4">
-	</video>
+$encrypted_url = openssl_encrypt($url, $cipher, $key, OPENSSL_RAW_DATA, $iv); // Cifrar la URL
 
+$iv = base64_encode($iv); // Convertir el vector de inicialización a base64
+$encrypted_url = base64_encode($encrypted_url); // Convertir la URL cifrada a base64
 
-';
+// Construir la URL cifrada
+$final_url = "https://jhancarlos301.github.io/repro/index.php" . urlencode($encrypted_url) . "&iv=" . urlencode($iv);
+
+echo "URL cifrada: " . $final_url;
 ?>
